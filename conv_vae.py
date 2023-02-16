65cd7ed5d0a6ad6c5bae11de01d00b2e13e816a2
Stephen <smulligan@pp.studio>
2023-02-16 11:25:11 +0100

    Added new cVAE module and new loss function

--- conv_vae.py
+++ conv_vae.py
@@ -3,10 +3,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from network_blocks import Encoder, Decoder
 
-###################################################
-### Convolutional Variational Autoencoder Class ###
-###################################################
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+########################################################
+### Simple Conditional Variational Autoencoder Class ###
+########################################################
 
 class simple_VAE(nn.Module):
     def __init__(self, imgChannels=1, featureDim = 32*20*20, zDim=256):
@@ -62,5 +65,41 @@ class simple_VAE(nn.Module):
         return out, mu, logVar
     
 
+#################################################
+### Conditional Variational Autoencoder Class ###
+#################################################
+
+class cVAE(nn.Module):
+    def __init__(self, shape, nclass, nhid = 16, ncond =16):
+        super(cVAE, self).__init__()
+        self.dim = nhid
+        self.encoder = Encoder(shape, nhid, ncond = ncond)
+        self.decoder = Decoder(shape, nhid, ncond = ncond)
+        self.label_embedding = nn.Embedding(nclass, ncond)
+
+    def sampling(self, mean, logvar):
+        eps = torch.randn(mean.shape).to(device)
+        sigma = 0.5 * torch.exp(logvar)
+        return mean + eps * sigma
+
+    def forward(self, x, y):
+        y = self.label_embedding(y)
+        mean, logvar = self.encoder(x, y)
+        z = self.sampling(mean, logvar)
+        return self.decoder(z, y), mean, logvar
+
+    def generate(self, class_idx):
+        if (type(class_idx) is int):
+            class_idx = torch.tensor(class_idx)
+        class_idx = class_idx.to(device)
+        if (len(class_idx.shape) == 0):
+            batch_size = None
+            class_idx = class_idx.unsqueeze(0)
+            z = torch.randn((1, self.dim)).to(device)
+        y = self.label_embeddings(class_idx)
+        res = self.decoder(z, y)
+        if not batch_size:
+            res = res.squeeze(0)
+        return res
 
 
--- losses.py
+++ losses.py
@@ -16,3 +16,11 @@ def bin_cross_entropy(out, imgs):
 
 def vae_loss(out, imgs, mu, logVar):
     return bin_cross_entropy(out, imgs) + kl_div(mu, logVar)
+
+### loss for updated VAEs
+
+BCE_loss = nn.BCELoss(reduction = "sum")
+def new_vae_loss(X, X_hat, mean, logvar):
+    reconstruction_loss = BCE_loss(X_hat, X)
+    KL_div = 0.5 * torch.sum(-1 - logvar + torch.exp(logvar) + mean**2)
+    return reconstruction_loss + KL_div
