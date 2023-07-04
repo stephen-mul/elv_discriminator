d34ee94a49a73bdfe19a8e786213475c7eb5fdd8
Stephen <smulligan@pp.studio>
2023-07-04 15:16:12 +0200

    basic discrimanator architecture

--- discriminator.py
+++ discriminator.py
@@ -3,11 +3,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import one_hot_encoder
 
 ###################################
 ### Simple discriminator module ###
 ###################################
 
 class discriminator(nn.Module):
-    def __init__(self, ):
+    def __init__(self, img_channels=1, n_classes=10):
+        ### Initialise one hot enconder ##
+        
+        ### Initialise layers ###
+        self.Conv1 = nn.Conv2d(img_channels, 16, 3)
+        self.FC1 = nn.Linear(10816, n_classes)
+
+    def forward(self, image):
+        x = self.Conv1(image)
+        x = F.relu(x)
+        x = torch.flatten(x, 1)
+        x = self.FC1(x)
+        output = F.log_softmax(x)
+
+        return output
+
+
+
